383ae9cec1c04673de2b7643130da7fc9d27fc9f
Abdullah Al Jubair <jubair2810@gmail.com>
2025-11-28 19:09:17 +0600

    added:user scheam

---
+++ src/users/schema/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, trim: true, unique: true })
+  email: string;
+  @Prop({ required: true, trim: true })
+  name: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ role: 'user' })
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.index({ email: 1 });
