dc462313d7354f313b9c8d1d3f573a5e970cdbd5
Abdullah Al Jubair <jubair2810@gmail.com>
2025-11-28 22:35:43 +0600

    enable app services

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -39,5 +41,7 @@ import { AuthModule } from './auth/auth.module';
     NotesModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
