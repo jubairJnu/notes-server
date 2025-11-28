2244b386509d6b81310c94c0b4569b0cd8125bfe
Abdullah Al Jubair <jubair2810@gmail.com>
2025-11-28 18:20:39 +0600

    added:config and connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { NotesModule } from './notes/notes.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config';
 
 @Module({
-  imports: [UsersModule, NotesModule],
+  imports: [
+    MongooseModule.forRoot(config.db_url as string),
+    UsersModule,
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  db_url: process.env.DB_URL,
+  port: process.env.PORT,
+  node_env: process.env.NODE_ENV,
+};
